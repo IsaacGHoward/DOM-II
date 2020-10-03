925c5836ee5a11156e189ccadfe545465a54b06b
IsaacGHoward <howardisaac100@gmail.com>
2020-10-02 17:14:48 -0700

    Added first 2 event listeners, mouseover and mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+//First 2 event listeners
+const h2s = document.querySelectorAll("h2");
+h2s.forEach((h2) => {
+    h2.addEventListener("mouseover", (e) => {
+        h2.style.backgroundColor = "Yellow";
+    })
+    h2.addEventListener("mouseout", (e) => {
+        h2.style.backgroundColor = "White";
+    })
+})
