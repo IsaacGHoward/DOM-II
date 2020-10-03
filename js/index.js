314d878744640025fa683e22bddbd5df380fb8e5
IsaacGHoward <howardisaac100@gmail.com>
2020-10-02 17:30:55 -0700

    Added dblclick event listener, and used stopPropagation

--- js/index.js
+++ js/index.js
@@ -10,3 +10,26 @@ h2s.forEach((h2) => {
         h2.style.backgroundColor = "White";
     })
 })
+
+//Event listeners for text content
+const text_content = document.querySelectorAll(".text-content");
+text_content.forEach((text) => {
+    text.addEventListener("mouseover", (e) => {
+        text.style.backgroundColor = "Blue";
+    })
+    text.addEventListener("mouseout", (e) => {
+        text.style.backgroundColor = "White";
+    })
+})
+
+//Added listener for dblclick, and I stopped propagation
+const images = document.querySelectorAll("img");
+images.forEach((image) => {
+    image.addEventListener("dblclick", (e) => {
+        alert("You clicked : " + e.target.alt);
+        e.stopPropagation();
+    })
+})
+document.addEventListener("dblclick", (e) => {
+    alert("You clicked the document!");
+})
